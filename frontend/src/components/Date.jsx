1a66cce74a78cb8c8a6ed64b441ce734ed564ddf
Girish-Gaurav-Sharma <135326401+Girish-Gaurav-Sharma@users.noreply.github.com>
2024-10-08 12:38:54 +0530

    gg

--- frontend/src/components/Date.jsx
+++ frontend/src/components/Date.jsx
@@ -18,48 +18,48 @@ const SatelliteDataDisplay = () => {
 			},
 			bands: {
 				B2: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/a94ca6c8d61b5334bdf74f9899bea4e6-11f4cb4ce860abb30ec679b01ca8b253:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.06317611104764083,
 				},
 				B3: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/ddd4122d8714b4e849c4c2bc02b24379-1d35bbc10659c463b70387f37dcc7b60:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.0957873757568834,
 				},
 				B4: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/2757230e804c6a4a1c9df72d54aae78c-86ed1cf73b769e03324d73129d0c2400:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.10223739289386496,
 				},
 				B5: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/8ec27a8e2ff428aee38b4093c7cdc20d-75ee783f134d7571ccd128193528646a:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.20293394264823492,
 				},
 				B6: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/a52a001ed9210b9e1fddcdb14917899d-62bb6a6a842de78d3ecbcc43903d2711:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.19586324688678175,
 				},
 				B7: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/0f9fe9f3d17f6880ae1646e660b425ef-7b641d60887feffaf9a6554f8073ccb9:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.14944526448074952,
 				},
 				NDVI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/aac45a96f30ffac5923377163e6b55d5-a488cd22b718844d0fb64f45f414f21c:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.3362116660830717,
 				},
 				NDWI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/444481879b49f1d6c5d1b9e8364801ae-cdea8718d7a64a1d1196ad2563d7c27d:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: -0.3602293033064506,
 				},
 				EVI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/17591aa0ffcc240139dd8c410b4bf87d-9261d64c2d5237ea48c556082af0fd4c:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.18869075070967348,
 				},
 			},
 			composites: {
 				RGB: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/f6e6ac8cd02e14c1928f88e1821fa32b-4c72b8b122a11065a3650b5a59c8be04:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 				},
 				False_Color: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/820c832033b58e60a8fc331fbf0e79b2-49bed36ae87722186cc82242d74f62f8:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 				},
 			},
 		},
@@ -79,48 +79,48 @@ const SatelliteDataDisplay = () => {
 			},
 			bands: {
 				B2: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/a94ca6c8d61b5334bdf74f9899bea4e6-11f4cb4ce860abb30ec679b01ca8b253:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.06317611104764083,
 				},
 				B3: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/ddd4122d8714b4e849c4c2bc02b24379-1d35bbc10659c463b70387f37dcc7b60:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.0957873757568834,
 				},
 				B4: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/2757230e804c6a4a1c9df72d54aae78c-86ed1cf73b769e03324d73129d0c2400:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.10223739289386496,
 				},
 				B5: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/8ec27a8e2ff428aee38b4093c7cdc20d-75ee783f134d7571ccd128193528646a:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.20293394264823492,
 				},
 				B6: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/a52a001ed9210b9e1fddcdb14917899d-62bb6a6a842de78d3ecbcc43903d2711:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.19586324688678175,
 				},
 				B7: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/0f9fe9f3d17f6880ae1646e660b425ef-7b641d60887feffaf9a6554f8073ccb9:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.14944526448074952,
 				},
 				NDVI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/aac45a96f30ffac5923377163e6b55d5-a488cd22b718844d0fb64f45f414f21c:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.3362116660830717,
 				},
 				NDWI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/444481879b49f1d6c5d1b9e8364801ae-cdea8718d7a64a1d1196ad2563d7c27d:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: -0.3602293033064506,
 				},
 				EVI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/17591aa0ffcc240139dd8c410b4bf87d-9261d64c2d5237ea48c556082af0fd4c:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.18869075070967348,
 				},
 			},
 			composites: {
 				RGB: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/f6e6ac8cd02e14c1928f88e1821fa32b-4c72b8b122a11065a3650b5a59c8be04:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 				},
 				False_Color: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/820c832033b58e60a8fc331fbf0e79b2-49bed36ae87722186cc82242d74f62f8:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 				},
 			},
 		},
@@ -140,48 +140,48 @@ const SatelliteDataDisplay = () => {
 			},
 			bands: {
 				B2: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/a94ca6c8d61b5334bdf74f9899bea4e6-11f4cb4ce860abb30ec679b01ca8b253:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.06317611104764083,
 				},
 				B3: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/ddd4122d8714b4e849c4c2bc02b24379-1d35bbc10659c463b70387f37dcc7b60:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.0957873757568834,
 				},
 				B4: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/2757230e804c6a4a1c9df72d54aae78c-86ed1cf73b769e03324d73129d0c2400:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.10223739289386496,
 				},
 				B5: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/8ec27a8e2ff428aee38b4093c7cdc20d-75ee783f134d7571ccd128193528646a:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.20293394264823492,
 				},
 				B6: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/a52a001ed9210b9e1fddcdb14917899d-62bb6a6a842de78d3ecbcc43903d2711:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.19586324688678175,
 				},
 				B7: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/0f9fe9f3d17f6880ae1646e660b425ef-7b641d60887feffaf9a6554f8073ccb9:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.14944526448074952,
 				},
 				NDVI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/aac45a96f30ffac5923377163e6b55d5-a488cd22b718844d0fb64f45f414f21c:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.3362116660830717,
 				},
 				NDWI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/444481879b49f1d6c5d1b9e8364801ae-cdea8718d7a64a1d1196ad2563d7c27d:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: -0.3602293033064506,
 				},
 				EVI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/17591aa0ffcc240139dd8c410b4bf87d-9261d64c2d5237ea48c556082af0fd4c:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.18869075070967348,
 				},
 			},
 			composites: {
 				RGB: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/f6e6ac8cd02e14c1928f88e1821fa32b-4c72b8b122a11065a3650b5a59c8be04:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 				},
 				False_Color: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/820c832033b58e60a8fc331fbf0e79b2-49bed36ae87722186cc82242d74f62f8:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 				},
 			},
 		},
@@ -201,48 +201,48 @@ const SatelliteDataDisplay = () => {
 			},
 			bands: {
 				B2: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/a94ca6c8d61b5334bdf74f9899bea4e6-11f4cb4ce860abb30ec679b01ca8b253:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.06317611104764083,
 				},
 				B3: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/ddd4122d8714b4e849c4c2bc02b24379-1d35bbc10659c463b70387f37dcc7b60:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.0957873757568834,
 				},
 				B4: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/2757230e804c6a4a1c9df72d54aae78c-86ed1cf73b769e03324d73129d0c2400:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.10223739289386496,
 				},
 				B5: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/8ec27a8e2ff428aee38b4093c7cdc20d-75ee783f134d7571ccd128193528646a:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.20293394264823492,
 				},
 				B6: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/a52a001ed9210b9e1fddcdb14917899d-62bb6a6a842de78d3ecbcc43903d2711:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.19586324688678175,
 				},
 				B7: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/0f9fe9f3d17f6880ae1646e660b425ef-7b641d60887feffaf9a6554f8073ccb9:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.14944526448074952,
 				},
 				NDVI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/aac45a96f30ffac5923377163e6b55d5-a488cd22b718844d0fb64f45f414f21c:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.3362116660830717,
 				},
 				NDWI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/444481879b49f1d6c5d1b9e8364801ae-cdea8718d7a64a1d1196ad2563d7c27d:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: -0.3602293033064506,
 				},
 				EVI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/17591aa0ffcc240139dd8c410b4bf87d-9261d64c2d5237ea48c556082af0fd4c:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.18869075070967348,
 				},
 			},
 			composites: {
 				RGB: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/f6e6ac8cd02e14c1928f88e1821fa32b-4c72b8b122a11065a3650b5a59c8be04:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 				},
 				False_Color: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/820c832033b58e60a8fc331fbf0e79b2-49bed36ae87722186cc82242d74f62f8:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 				},
 			},
 		},
@@ -262,48 +262,48 @@ const SatelliteDataDisplay = () => {
 			},
 			bands: {
 				B2: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/a94ca6c8d61b5334bdf74f9899bea4e6-11f4cb4ce860abb30ec679b01ca8b253:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.06317611104764083,
 				},
 				B3: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/ddd4122d8714b4e849c4c2bc02b24379-1d35bbc10659c463b70387f37dcc7b60:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.0957873757568834,
 				},
 				B4: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/2757230e804c6a4a1c9df72d54aae78c-86ed1cf73b769e03324d73129d0c2400:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.10223739289386496,
 				},
 				B5: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/8ec27a8e2ff428aee38b4093c7cdc20d-75ee783f134d7571ccd128193528646a:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.20293394264823492,
 				},
 				B6: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/a52a001ed9210b9e1fddcdb14917899d-62bb6a6a842de78d3ecbcc43903d2711:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.19586324688678175,
 				},
 				B7: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/0f9fe9f3d17f6880ae1646e660b425ef-7b641d60887feffaf9a6554f8073ccb9:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.14944526448074952,
 				},
 				NDVI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/aac45a96f30ffac5923377163e6b55d5-a488cd22b718844d0fb64f45f414f21c:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.3362116660830717,
 				},
 				NDWI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/444481879b49f1d6c5d1b9e8364801ae-cdea8718d7a64a1d1196ad2563d7c27d:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: -0.3602293033064506,
 				},
 				EVI: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/17591aa0ffcc240139dd8c410b4bf87d-9261d64c2d5237ea48c556082af0fd4c:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 					average: 0.18869075070967348,
 				},
 			},
 			composites: {
 				RGB: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/f6e6ac8cd02e14c1928f88e1821fa32b-4c72b8b122a11065a3650b5a59c8be04:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 				},
 				False_Color: {
-					url: 'https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/820c832033b58e60a8fc331fbf0e79b2-49bed36ae87722186cc82242d74f62f8:getPixels',
+					url: 'https://custom-scripts.sentinel-hub.com/custom-scripts/hls/true_color/fig/fig1.jpeg',
 				},
 			},
 		},
